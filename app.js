70a84b81682f7f32449f9d65b80799b318014c75
Brian Zimorowicz <brianz@email.unc.edu>
2013-04-19 11:51:38 -0400

    Revert "changed everything"
    This reverts commit ed60ef6961c3feabbe24ff3400d9b3aeecd86df2.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
-var USER_OR_GROUP_NAME = 'nestofsaws'; // TODO: Insert GitHub username or group name.
+var USER_OR_GROUP_NAME = ''; // TODO: Insert GitHub username or group name.
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -30,17 +30,17 @@ var db = utils.connectToDatabase(USER_OR_GROUP_NAME);
 // Example of handling PUT to create or update a resource. /////////////////////
 // Here we create or update an item using the ID specified in the URI. /////////
 ////////////////////////////////////////////////////////////////////////////////
-app.put('/titles/:id',      // TODO: change to suit your URI design.
+app.put('/parties/:id',      // TODO: change to suit your URI design.
   function(req, res) {
   
     // Get the item ID from the URI.
     var item_id = req.params.id;
 
     // Get the item info that was PUT from the input form.
-    // See the form in `views/list-titles.ejs`.
+    // See the form in `views/list-parties.ejs`.
     var item = req.body.item;
     
-    item.type = 'title'; // TODO: change to the type of item you want
+    item.type = 'party'; // TODO: change to the type of item you want
 
     // Save the new item to the database, specifying the ID.
     db.save(item_id, item, function(err) {
@@ -56,12 +56,12 @@ app.put('/titles/:id',      // TODO: change to suit your URI design.
 
 ////////////////////////////////////////////////////////////////////////////////
 // Example of handling GET of a "collection" resource. /////////////////////////
-// Here we list all items of type `title`. /////////////////////////////////////
+// Here we list all items of type `party`. /////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-app.get('/titles/',         // TODO: change to suit your URI design. 
+app.get('/parties/',         // TODO: change to suit your URI design. 
   function(req, res) {
 
-    var item_type = 'title'; // TODO: change to the type of item you want.
+    var item_type = 'party'; // TODO: change to the type of item you want.
 
     // Get all items of the specified type from the database.
     db.getAll(item_type, function(err, items) {
@@ -72,7 +72,7 @@ app.get('/titles/',         // TODO: change to suit your URI design.
       // Otherwise, use the returned data to render an HTML page.
       else {
         res.render(
-          'list-titles',   // TODO: change to the name of your HTML template.
+          'list-parties',   // TODO: change to the name of your HTML template.
           { items: items }
         );
       }
@@ -84,14 +84,14 @@ app.get('/titles/',         // TODO: change to suit your URI design.
 // Example of handling POST to create a resource. //////////////////////////////
 // Here we create an item and allow the ID to be created automatically. ////////
 ////////////////////////////////////////////////////////////////////////////////
-app.post('/issues/', // TODO: change to suit your URI design.
+app.post('/candidates/', // TODO: change to suit your URI design.
   function(req, res) {
   
     // Get the item info that was POSTed from the input form.
-    // See the form in `views/one-title.ejs`.
+    // See the form in `views/one-party.ejs`.
     var item = req.body.item;
 
-    item.type = 'issue'; // TODO: change to the type of item you want
+    item.type = 'candidate'; // TODO: change to the type of item you want
 
     // Save the new item to the database. (No ID specified, it will be created.)
     db.save(item, function(err) {
@@ -109,17 +109,17 @@ app.post('/issues/', // TODO: change to suit your URI design.
 // Another example of handling PUT to update a resource. ///////////////////////
 // Here we update an item using the ID specified in the URI. ///////////////////
 ////////////////////////////////////////////////////////////////////////////////
-app.put('/issues/:id', // TODO: change to suit your URI design.
+app.put('/candidates/:id', // TODO: change to suit your URI design.
   function(req, res) {
   
     // Get the item ID from the URI.
     var item_id = req.params.id;
 
     // Get the item info that was PUT from the input form.
-    // See the form in `views/one-issue.ejs`.
+    // See the form in `views/one-candidate.ejs`.
     var item = req.body.item;
 
-    item.type = 'issue'; // TODO: change to the type of item you want
+    item.type = 'candidate'; // TODO: change to the type of item you want
 
     // Save the new item to the database, specifying the ID.
     db.save(item_id, item, function(err) {
@@ -137,10 +137,10 @@ app.put('/issues/:id', // TODO: change to suit your URI design.
 // Another example of handling GET of a "collection" resource. /////////////////
 // This time we support filtering the list by some criteria (i.e. searching). //
 ////////////////////////////////////////////////////////////////////////////////
-app.get('/issues/',          // TODO: change to suit your URI design. 
+app.get('/candidates/',          // TODO: change to suit your URI design. 
   function(req, res) {
 
-    var item_type = 'issue'; // TODO: change to the type of item you want.
+    var item_type = 'candidate'; // TODO: change to the type of item you want.
 
     // Get items of the specified type that match the query.
     db.getSome(item_type, req.query, function(err, items) {
@@ -151,7 +151,7 @@ app.get('/issues/',          // TODO: change to suit your URI design.
       // Otherwise, use the returned data to render an HTML page.
       else {
         res.render(
-          'list-issues', // TODO: change to the name of your HTML template.
+          'list-candidates', // TODO: change to the name of your HTML template.
           { items: items }
         );
       }
@@ -164,10 +164,10 @@ app.get('/issues/',          // TODO: change to suit your URI design.
 // This handler is more complicated, because we want to show not only the //////
 // item requested, but also links to a set of related items. ///////////////////
 ////////////////////////////////////////////////////////////////////////////////
-app.get('/titles/:id',      // TODO: change to suit your URI design.
+app.get('/parties/:id',      // TODO: change to suit your URI design.
   function(req, res) {
 
-    var item_type = 'title'; // TODO: change to the type of item you want.
+    var item_type = 'party'; // TODO: change to the type of item you want.
 
     // Get the item ID from the URI.
     var item_id = req.params.id;
@@ -184,10 +184,10 @@ app.get('/titles/:id',      // TODO: change to suit your URI design.
       // Otherwise, get the related items associated with this item.
       else {
         
-        var related_type = 'issue'; // TODO: change to type of related item.
+        var related_type = 'candidate'; // TODO: change to type of related item.
 
         // Set our query to find the items related to the requested item.
-        req.query.title = item_id; // TODO: change `title` to reflect the
+        req.query.party = item_id; // TODO: change `party` to reflect the
                                    // relation between the item fetched above
                                    // and the related items to be fetched below.
 
@@ -200,7 +200,7 @@ app.get('/titles/:id',      // TODO: change to suit your URI design.
           // Otherwise, use the returned data to render an HTML page.
           else {
             res.render(
-            'one-title', // TODO: change to the name of your HTML template.
+            'one-party', // TODO: change to the name of your HTML template.
               { item: item, related_items: items }
             );
           }
@@ -216,10 +216,10 @@ app.get('/titles/:id',      // TODO: change to suit your URI design.
 // item requested, but also a list of potential related items, so that users ///
 // can select from a list when updating the item. //////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-app.get('/issues/:id',       // TODO: change to suit your URI design.
+app.get('/candidates/:id',       // TODO: change to suit your URI design.
   function(req, res) {
 
-    var item_type = 'issue'; // TODO: change to the type of item you want.
+    var item_type = 'candidate'; // TODO: change to the type of item you want.
 
     // Get the item ID from the URI.
     var item_id = req.params.id;
@@ -236,7 +236,7 @@ app.get('/issues/:id',       // TODO: change to suit your URI design.
       // Otherwise, get the items potentially related to this item.
       else {
         
-        var related_type = 'title'; // TODO: change to type of related item.
+        var related_type = 'party'; // TODO: change to type of related item.
 
         // Get all items of the specified related type.
         db.getAll(related_type, function(err, items) {
@@ -247,7 +247,7 @@ app.get('/issues/:id',       // TODO: change to suit your URI design.
           // Otherwise, use the returned data to render an HTML page.
           else {
             res.render(
-              'one-issue', // TODO: change to name of your HTML template.
+              'one-candidate', // TODO: change to name of your HTML template.
               { item: item, related_items: items }
             );
           }
